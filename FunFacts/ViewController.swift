ebc372d4a2a968c05adc188ca74bf4af8977b0be
David Muro II <dmuro2@gmail.com>
2015-01-19 18:31:07 -0800

    First commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by David Muro on 1/2/15.
+//  Copyright (c) 2015 David Muro II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactLabel.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+
+}
+
---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+var fruitsArray : [String] = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
